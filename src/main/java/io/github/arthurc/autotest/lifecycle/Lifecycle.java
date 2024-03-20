3b2c12ba325d53526ec63cb113f1b78b006edcab
Arthur Hartwig Carlsson <arthur@kiron.net>
2024-03-20 22:45:30 +0100

    Add nested lifecycles

--- src/main/java/io/github/arthurc/autotest/lifecycle/Lifecycle.java
+++ src/main/java/io/github/arthurc/autotest/lifecycle/Lifecycle.java
@@ -3,6 +3,9 @@
  */
 package io.github.arthurc.autotest.lifecycle;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * A lifecycle is a thread scoped execution of code that has a start and an end.
  *
@@ -10,19 +13,44 @@ package io.github.arthurc.autotest.lifecycle;
  * @since 1.0.0
  */
 public abstract class Lifecycle {
+	private static final ThreadLocal<Deque<Lifecycle>> CALLSTACK = new InheritableThreadLocal<>() {
+		@Override
+		protected Deque<Lifecycle> initialValue() {
+			return new LinkedList<>();
+		}
+
+		@Override
+		protected Deque<Lifecycle> childValue(Deque<Lifecycle> parentValue) {
+			return new LinkedList<>(parentValue);
+		}
+	};
+
+	private Lifecycle parent;
+
 	/**
 	 * Runs a task within the lifecycle. The lifecycle publishes events before and after the task is run.
 	 *
 	 * @param task The task to run.
 	 */
 	public void run(Runnable task) {
-		onLifecycleEvent(new LifecycleEvent.BeforeBegin(this));
-		onLifecycleEvent(new LifecycleEvent.AfterBegin(this));
+		publish(new LifecycleEvent.BeforeBegin(this));
+		this.parent = CALLSTACK.get().peek();
+		CALLSTACK.get().push(this);
+		publish(new LifecycleEvent.AfterBegin(this));
 		try {
 			task.run();
 		} finally {
-			onLifecycleEvent(new LifecycleEvent.BeforeEnd(this));
-			onLifecycleEvent(new LifecycleEvent.AfterEnd(this));
+			publish(new LifecycleEvent.BeforeEnd(this));
+			CALLSTACK.get().pop();
+			this.parent = null;
+			publish(new LifecycleEvent.AfterEnd(this));
+		}
+	}
+
+	private void publish(LifecycleEvent event) {
+		onLifecycleEvent(event);
+		if (this.parent != null) {
+			this.parent.publish(event);
 		}
 	}
 
--- src/test/java/io/github/arthurc/autotest/lifecycle/LifecycleTest.java
+++ src/test/java/io/github/arthurc/autotest/lifecycle/LifecycleTest.java
@@ -5,6 +5,9 @@ package io.github.arthurc.autotest.lifecycle;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoSettings;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
@@ -15,15 +18,19 @@ class TestLifecycle extends Lifecycle {
 	}
 }
 
+@MockitoSettings
 class LifecycleTest {
+
+	@Mock
+	private Runnable task;
 
 	@Nested
 	class A_Lifecycle_that_is_run {
+		@Spy
+		private TestLifecycle lifecycle = new TestLifecycle();
+
 		@Test
 		void Publishes_lifeycle_events_in_the_correct_order() {
-			var lifecycle = spy(new TestLifecycle());
-			var task = mock(Runnable.class);
-
 			lifecycle.run(task);
 
 			var inOrder = inOrder(lifecycle, task);
@@ -36,15 +43,54 @@ class LifecycleTest {
 
 		@Test
 		void Publishes_lifecycle_events_even_when_the_task_throws_an_exception() {
-			var lifecycle = spy(new TestLifecycle());
-			var task = (Runnable) () -> {
-				throw new RuntimeException();
-			};
+			doThrow(new RuntimeException()).when(task).run();
 
 			assertThatThrownBy(() -> lifecycle.run(task)).isInstanceOf(RuntimeException.class);
 
 			verify(lifecycle).onLifecycleEvent(new LifecycleEvent.BeforeEnd(lifecycle));
 			verify(lifecycle).onLifecycleEvent(new LifecycleEvent.AfterEnd(lifecycle));
+		}
+	}
+
+	@Nested
+	class A_Lifecycle_that_is_nested {
+
+		@Spy
+		private TestLifecycle outer = new TestLifecycle();
+
+		@Spy
+		private TestLifecycle inner = new TestLifecycle();
+
+		@Test
+		void Publishes_lifecycle_events_in_the_correct_order_for_nested_lifecycles() {
+			outer.run(() -> inner.run(task));
+
+			var inOrder = inOrder(outer, inner, task);
+			inOrder.verify(outer).onLifecycleEvent(new LifecycleEvent.BeforeBegin(outer));
+			inOrder.verify(outer).onLifecycleEvent(new LifecycleEvent.AfterBegin(outer));
+			inOrder.verify(inner).onLifecycleEvent(new LifecycleEvent.BeforeBegin(inner));
+			inOrder.verify(inner).onLifecycleEvent(new LifecycleEvent.AfterBegin(inner));
+			inOrder.verify(outer).onLifecycleEvent(new LifecycleEvent.AfterBegin(inner));
+			inOrder.verify(task).run();
+			inOrder.verify(inner).onLifecycleEvent(new LifecycleEvent.BeforeEnd(inner));
+			inOrder.verify(outer).onLifecycleEvent(new LifecycleEvent.BeforeEnd(inner));
+			inOrder.verify(inner).onLifecycleEvent(new LifecycleEvent.AfterEnd(inner));
+			inOrder.verify(outer).onLifecycleEvent(new LifecycleEvent.BeforeEnd(outer));
+			inOrder.verify(outer).onLifecycleEvent(new LifecycleEvent.AfterEnd(outer));
+		}
+
+		@Test
+		void Does_not_publish_BeforeBegin_to_parent_lifecycles() {
+			outer.run(() -> inner.run(task));
+
+			verify(outer, never()).onLifecycleEvent(new LifecycleEvent.BeforeBegin(inner));
+		}
+
+		@Test
+		void Does_not_publish_AfterEnd_to_parent_lifecycles() {
+			outer.run(() -> inner.run(task));
+
+			verify(outer, never()).onLifecycleEvent(new LifecycleEvent.AfterEnd(inner));
 		}
 
 	}
