e49c27d9ec95ba62b969e339e9d5e3b5a2a4a2a9
Arthur Hartwig Carlsson <arthur@kiron.net>
2024-03-22 22:49:47 +0100

    Adds Target=TYPE for @AutotestTest
    It only makes sense to have the annotations on type.
    Just like @SpringBootTest.

--- src/main/java/io/github/arthurc/autotest/AutotestTest.java
+++ src/main/java/io/github/arthurc/autotest/AutotestTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
@@ -20,6 +22,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * @since 1.0.0
  */
 @Retention(RUNTIME)
+@Target(TYPE)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Tag(AutotestTest.TAG)
 @ExtendWith(TestContextCleanupExtension.class)
